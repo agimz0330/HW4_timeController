24b476cc13b01fb434b0f7908f09bc5cfa375ad9
c60603 <c60603@gmail.com>
2020-11-04 16:22:53 +0800

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by User08 on 2020/11/4.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
